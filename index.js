512fa80744f9eb2e2c68025675f9cc73cc4a9b69
Denis <mugambimuthuri0@gmail.com>
2024-03-27 09:27:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+console.log(multiply)
+
+let counter = 0;
+counter--;
+console.log(counter);
+--counter;
+console.log(counter)
+
+let random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+
+let num3 = 24
+let num4 = 6
+let mod = (num3/num4)
+console.log(mod)
+
+let max = Math.max(4*5);
+console.log(max)
